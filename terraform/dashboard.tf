256307accd8ea3dd3016a23fa485a40b65035a69
Yuri <yuriferre@gmail.com>
2024-06-10 00:00:14 -0300

    update files

--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -23,32 +23,13 @@ resource "aws_cloudwatch_dashboard" "ecs_dashboard" {
         }
       },
       {
-        type = "metric",
-        x = 12,
-        y = 0,
-        width = 12,
-        height = 6,
-        properties = {
-          metrics = [
-            [ "AWS/ECS", "CPUUtilization", "ClusterName", var.cluster_name ],
-            [ ".", "MemoryUtilization", ".", "." ]
-          ],
-          view = "timeSeries",
-          stacked = false,
-          region = var.region,
-          period = 300,
-          stat = "Average",
-          title = "ECS Cluster CPU and Memory Utilization"
-        }
-      },
-      {
         type = "log",
         x = 0,
         y = 6,
         width = 24,
         height = 6,
         properties = {
-          query = "fields @timestamp, @message | sort @timestamp desc | limit 20",
+          query = "fields @timestamp, @message | sort @timestamp desc | limit 200",
           logGroupNames = ["/ecs/flask-app"],
           region = var.region,
           title = "Recent Log Events"
