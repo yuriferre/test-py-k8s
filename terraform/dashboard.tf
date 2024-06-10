ddd4f077d433dfa746c9f2f3666bfa186ac40a35
Yuri <yuriferre@gmail.com>
2024-06-10 00:04:42 -0300

    update files

--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -1,38 +1,57 @@
 resource "aws_cloudwatch_dashboard" "ecs_dashboard" {
-  dashboard_name = "ECS-Dashboard"
+  dashboard_name = "Flask-ECS-Dashboard"
 
   dashboard_body = jsonencode({
     widgets = [
       {
-        type = "metric",
-        x = 0,
-        y = 0,
-        width = 12,
-        height = 6,
-        properties = {
-          metrics = [
+        "type": "metric",
+        "x": 0,
+        "y": 0,
+        "width": 12,
+        "height": 6,
+        "properties": {
+          "metrics": [
             [ "AWS/ECS", "CPUUtilization", "ClusterName", var.cluster_name, "ServiceName", var.service_name ],
             [ ".", "MemoryUtilization", ".", ".", ".", "." ]
           ],
-          view = "timeSeries",
-          stacked = false,
-          region = var.region,
-          period = 300,
-          stat = "Average",
-          title = "ECS Service CPU and Memory Utilization"
+          "view": "timeSeries",
+          "stacked": false,
+          "region": var.region,
+          "period": 300,
+          "stat": "Average",
+          "title": "ECS Service CPU and Memory Utilization"
         }
       },
       {
-        type = "log",
-        x = 0,
-        y = 6,
-        width = 24,
-        height = 6,
-        properties = {
-          query = "fields @timestamp, @message | sort @timestamp desc | limit 200",
-          logGroupNames = ["/ecs/flask-app"],
-          region = var.region,
-          title = "Recent Log Events"
+        "type": "metric",
+        "x": 12,
+        "y": 0,
+        "width": 12,
+        "height": 6,
+        "properties": {
+          "metrics": [
+            [ "AWS/ECS", "CPUUtilization", "ClusterName", var.cluster_name ],
+            [ ".", "MemoryUtilization", ".", "." ]
+          ],
+          "view": "timeSeries",
+          "stacked": false,
+          "region": var.region,
+          "period": 300,
+          "stat": "Average",
+          "title": "ECS Cluster CPU and Memory Utilization"
+        }
+      },
+      {
+        "type": "log",
+        "x": 0,
+        "y": 6,
+        "width": 24,
+        "height": 6,
+        "properties": {
+          "query": "fields @timestamp, @message | sort @timestamp desc | limit 20",
+          "region": var.region,
+          "logGroupNames": ["/ecs/flask-app"],
+          "title": "Recent Log Events"
         }
       }
     ]
